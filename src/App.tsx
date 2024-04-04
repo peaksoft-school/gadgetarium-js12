2005ac9b7da2474166787d58343a2e908c4f09c8
NuriFront <toktogulovanurzanat@gmail.com>
2024-04-04 21:31:01 +0600

    added new select

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import LayoutUser from './pagesUser/components/layout/LayoutUser';
 import LayoutAdmin from './pagesAdmin/components/layout/LayoutAdmin';
 import LayoutAuth from './pagesAuth/components/layout/LayoutAuth';
+import CustomSelect from './UI/customSelect/CustomSelect';
 
 const App = () => {
 	return (
 		<>
+			<CustomSelect />
 			<Routes>
 				<Route path="/*" element={<LayoutUser />} />
 				<Route path="/auth/*" element={<LayoutAuth />} />
