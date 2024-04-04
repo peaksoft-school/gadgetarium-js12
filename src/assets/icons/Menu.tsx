f577389035addd7d347086b81c249f2859dd8a4b
esenturDev <nurlanuuluesentur@gmail.com>
2024-04-04 21:51:30 +0600

    added icons

---
+++ src/assets/icons/Menu.tsx
@@ -0,0 +1,65 @@
+export  const Menu = () => {
+	return (
+		<>
+			<svg
+				width="30"
+				height="30"
+				viewBox="0 0 30 30"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg">
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M27 24.2857V5.71429C27 4.7292 26.5786 3.78445 25.8284 3.08789C25.0783 2.39133 24.0609 2 23 2H7C5.93913 2 4.92172 2.39133 4.17157 3.08789C3.42143 3.78445 3 4.7292 3 5.71429V24.2857C3 25.2708 3.42143 26.2155 4.17157 26.9121C4.92172 27.6087 5.93913 28 7 28H23C24.0609 28 25.0783 27.6087 25.8284 26.9121C26.5786 26.2155 27 25.2708 27 24.2857V24.2857Z"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M12.332 10.048H20.6661"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M7.33203 10.048H8.99884"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M12.332 14.6913H20.6661"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M7.33203 14.6913H8.99884"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M12.332 19.3347H20.6661"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+				<path
+					d="M7.33203 19.3347H8.99884"
+					stroke="#384255"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+		</>
+	);
+};
+
