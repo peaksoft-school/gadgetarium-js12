4954e99168dc7bd9bd2fe8271c2501feed073270
Taalaibekova-Arslana <arslanataalajbekova@gmail.com>
2024-04-07 23:11:11 +0600

    added searchInput

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,68 +1,69 @@
 export { default as IconArrowRight } from './arrow-right';
 export { default as IconArrowLeft } from './arrow-left';
-export { Profile } from "./Profile";
-export { BankCardIcon } from "./BankCardIcon";
-export { BigDeleteIcon } from "./BigDeleteIcon";
-export { Buttoncolorfelt } from "./Buttoncolorfelt";
-export { Calendar } from "./Calendar";
-export { CameraIcon } from "./CameraIcon";
-export { CardIcon } from "./CardIcon";
-export { CardIcon2 } from "./CardIcon2";
-export { CarIcon } from "./CarIcon";
-export { Catalog } from "./Catalog";
-export { CertificateIcon } from "./CertificateIcon";
-export { DeleteIcon } from "./DeleteIcon";
-export { Downloads } from "./Downloads";
-export { EditIcon } from "./EditIcon";
-export { GoldIcon } from "./GoldIcon";
-export { IconBottom } from "./IconBottom";
-export { IconGallery } from "./IconGallery";
-export { KamazCarIcon } from "./KamazCarIcon";
-export { LocationIcon } from "./LocationIcon";
-export { Menu } from "./Menu";
-export { Microphone } from "./Microphone";
-export { ProfileColorwhite } from "./ProfileColorwhite";
-export { ScalesActive } from "./ScalesActive";
-export { ScalesGrey } from "./ScalesGrey";
-export { SmallDeleteIcon } from "./SmallDeleteIcon";
-export { ToolIcon } from "./ToolIcon";
-export { WalletIcon } from "./WalletIcon";
-export { AgreementIcon } from "./AgreementIcon";
-export { IconPurple } from "./IconPurple";
-export { Customer } from "./CustomerIcon";
-export { Desktop } from "./Desktop";
-export { Mobile } from "./Mobile";
-export { PropertyActive } from "./PropertyActive";
-export { PropertyClick } from "./PropertyClick";
-export {Frame} from './Frame'
-export {PropertyComponent} from './PropertyComponent'
-export {PropertyDefault} from './PropertyDefault'
-export {PropertyDefault2} from './PropertyDefault2'
-export {PropertyDefault3} from './PropertyDefault3'
-export {PropertyDefault4} from './PropertyDefault4'
-export {PropertyDefault5} from './PropertyDefault5'
-export {PropertyDefault7} from './PropertyDefault7'
-export {PropertyDefault8} from './PropertyDefault8'
-export {PropertyDefault9} from './PropertyDefault9'
-export {PropertyDefault11} from './PropertyDefault11'
-export {PropertyDefault12} from './PropertyDefault12'
-export {PropertyDefault13} from './PropertyDefault13'
-export {PropertyDefault14} from './PropertyDefault14'
-export {PropertyDefault15} from './PropertyDefault15'
-export {PropertyDefault16} from './PropertyDefault16'
-export {PropertyDefault17} from './PropertyDefault17'
-export {PropertyDefault19} from './PropertyDefault19'
-export {PropertyDefault20} from './PropertyDefault20'
-export {PropertyDefault22} from './PropertyDefault22'
-export {PropertyDefault24} from './PropertyDefault24'
-export {PropertyDefault25} from './PropertyDefault25'
-export {PropertyDefault26} from './PropertyDefault26'
-export {PropertyDefault_} from './PropertyDefault_'
-export {PropertyFrame2} from './PropertyFrame2'
-export {PropertyHover} from './PropertyHover'
-export {Property_Active} from './Property_Active'
-export {StateDown} from './StateDown'
-export {StateLeft} from './StateLeft'
-export {StateRight1} from './StateRight1'
-export {StateUp} from './StateUp'
-export {Watchalt} from './watchalt'
+export { Profile } from './Profile';
+export { BankCardIcon } from './BankCardIcon';
+export { BigDeleteIcon } from './BigDeleteIcon';
+export { Buttoncolorfelt } from './Buttoncolorfelt';
+export { Calendar } from './Calendar';
+export { CameraIcon } from './CameraIcon';
+export { CardIcon } from './CardIcon';
+export { CardIcon2 } from './CardIcon2';
+export { CarIcon } from './CarIcon';
+export { Catalog } from './Catalog';
+export { CertificateIcon } from './CertificateIcon';
+export { DeleteIcon } from './DeleteIcon';
+export { Downloads } from './Downloads';
+export { EditIcon } from './EditIcon';
+export { GoldIcon } from './GoldIcon';
+export { IconBottom } from './IconBottom';
+export { IconGallery } from './IconGallery';
+export { KamazCarIcon } from './KamazCarIcon';
+export { LocationIcon } from './LocationIcon';
+export { Menu } from './Menu';
+export { Microphone } from './Microphone';
+export { ProfileColorwhite } from './ProfileColorwhite';
+export { ScalesActive } from './ScalesActive';
+export { ScalesGrey } from './ScalesGrey';
+export { SmallDeleteIcon } from './SmallDeleteIcon';
+export { ToolIcon } from './ToolIcon';
+export { WalletIcon } from './WalletIcon';
+export { AgreementIcon } from './AgreementIcon';
+export { IconPurple } from './IconPurple';
+export { Customer } from './CustomerIcon';
+export { Desktop } from './Desktop';
+export { Mobile } from './Mobile';
+export { PropertyActive } from './PropertyActive';
+export { PropertyClick } from './PropertyClick';
+export { Frame } from './Frame';
+export { PropertyComponent } from './PropertyComponent';
+export { PropertyDefault } from './PropertyDefault';
+export { PropertyDefault2 } from './PropertyDefault2';
+export { PropertyDefault3 } from './PropertyDefault3';
+export { PropertyDefault4 } from './PropertyDefault4';
+export { PropertyDefault5 } from './PropertyDefault5';
+export { PropertyDefault7 } from './PropertyDefault7';
+export { PropertyDefault8 } from './PropertyDefault8';
+export { PropertyDefault9 } from './PropertyDefault9';
+export { PropertyDefault11 } from './PropertyDefault11';
+export { PropertyDefault12 } from './PropertyDefault12';
+export { PropertyDefault13 } from './PropertyDefault13';
+export { PropertyDefault14 } from './PropertyDefault14';
+export { PropertyDefault15 } from './PropertyDefault15';
+export { PropertyDefault16 } from './PropertyDefault16';
+export { PropertyDefault17 } from './PropertyDefault17';
+export { PropertyDefault19 } from './PropertyDefault19';
+export { PropertyDefault20 } from './PropertyDefault20';
+export { PropertyDefault22 } from './PropertyDefault22';
+export { PropertyDefault24 } from './PropertyDefault24';
+export { PropertyDefault25 } from './PropertyDefault25';
+export { PropertyDefault26 } from './PropertyDefault26';
+export { PropertyDefault_ } from './PropertyDefault_';
+export { PropertyFrame2 } from './PropertyFrame2';
+export { PropertyHover } from './PropertyHover';
+export { Property_Active } from './Property_Active';
+export { StateDown } from './StateDown';
+export { StateLeft } from './StateLeft';
+export { StateRight1 } from './StateRight1';
+export { StateUp } from './StateUp';
+export { Watchalt } from './watchalt';
+export { default as SearchIcon } from './SearchIcon';
