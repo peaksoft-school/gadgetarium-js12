b6038ac005b8ac2de8fa0d2d616be0a68f47fcbc
esenturDev <nurlanuuluesentur@gmail.com>
2024-04-08 20:55:23 +0600

    update icons

---
+++ src/assets/icons/icon-Text-i.tsx
@@ -0,0 +1,26 @@
+const texti = () => {
+	return (
+		<>
+			<svg
+				width="28"
+				height="28"
+				viewBox="0 0 28 28"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg">
+				<g clip-path="url(#clip0_1300_16946)">
+					<path
+						d="M17.5 23.3333H8.16663V21H11.5815L14.0501 6.99996H10.5V4.66663H19.8333V6.99996H16.4185L13.9498 21H17.5V23.3333Z"
+						fill="#292929"
+					/>
+				</g>
+				<defs>
+					<clipPath id="clip0_1300_16946">
+						<rect width="28" height="28" fill="white" />
+					</clipPath>
+				</defs>
+			</svg>
+		</>
+	);
+};
+
+export default texti
