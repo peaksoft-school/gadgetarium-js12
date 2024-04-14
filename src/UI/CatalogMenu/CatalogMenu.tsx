f17a3625354fe2a513dc5c21d070f3fb0447154b
esenturDev <nurlanuuluesentur@gmail.com>
2024-04-14 17:46:37 +0600

    create CatalogMenu

---
+++ src/UI/CatalogMenu/CatalogMenu.tsx
@@ -0,0 +1,13 @@
+import scss from './CatalogMenu.module.scss';
+
+const CatalogMenu = () => {
+  return (
+    <div className={scss.CatalogMenu}>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CatalogMenu
