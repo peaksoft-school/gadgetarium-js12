4d799b13ee9d9cedc99507c512519fcc6fbc3420
Taalaibekova-Arslana <arslanataalajbekova@gmail.com>
2024-04-15 13:01:56 +0600

    added header

--- src/pagesUser/components/layout/header/Header.tsx
+++ src/pagesUser/components/layout/header/Header.tsx
@@ -1,3 +1,4 @@
+import scss from './Header.module.scss';
 import {
 	IconBasket,
 	IconComparison,
@@ -8,17 +9,40 @@ import {
 	IconWhatsapp,
 	Profile
 } from '@/src/assets/icons';
-import scss from './Header.module.scss';
+import { UnorderedListOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import EightPoints from '@/src/assets/icons/eight-points';
 import CustomSearch from '@/src/UI/customInputs/customSearch/CustomSearch';
 import { useEffect, useState } from 'react';
 
+const links = [
+	{
+		label: 'Главная',
+		link: '/'
+	},
+	{
+		label: 'О магазине',
+		link: '/aboutstore'
+	},
+	{
+		label: 'Доставка',
+		link: '/delivery'
+	},
+	{
+		label: 'FAG',
+		link: '/fag'
+	},
+	{
+		label: 'Контакты',
+		link: '/contacts'
+	}
+];
+
 const Header = () => {
 	const [activePage, setActivePage] = useState('');
 	const [isMenu, setIsMenu] = useState(false);
-
 	const [isScrolled, setIsScrolled] = useState(false);
+	const [burgerMenu, setBurgerMenu] = useState(false);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -33,117 +57,27 @@ const Header = () => {
 	}, []);
 
 	return (
-		<header className={`${scss.Header} ${isScrolled ? scss.scrolled : ''}`}>
+		<header className={scss.Header}>
 			<div className="container">
 				<div className={scss.content}>
-					{/* //!HEADER FIRST */}
-					<div className={scss.headerFirst}>
-						<Link to="/">
+					<div className={scss.sup_header}>
+						<Link className={scss.logo} to="/">
 							<IconGadgetarium />
 						</Link>
-						<ul className={scss.pages}>
-							<li>
-								<Link
-									className={`${scss.link} ${activePage === '/' && scss.activeBackground}`}
-									to="/"
-									onClick={() => setActivePage('/')}
-								>
-									Главная
-								</Link>
-							</li>
-							<li>
-								<Link
-									className={`${scss.link} ${activePage === '/aboutstore' && scss.activeBackground}`}
-									to="/aboutstore"
-									onClick={() => setActivePage('/aboutstore')}
-								>
-									О магазине
-								</Link>
-							</li>
-							<li>
-								<Link
-									className={`${scss.link} ${activePage === '/delivery' && scss.activeBackground}`}
-									to="/delivery"
-									onClick={() => setActivePage('/delivery')}
-								>
-									Доставка
-								</Link>
-							</li>
-							<li>
-								<Link
-									className={`${scss.link} ${activePage === '/fag' && scss.activeBackground}`}
-									to="/fag"
-									onClick={() => setActivePage('/fag')}
-								>
-									FAG
-								</Link>
-							</li>
-							<li>
-								<Link
-									className={`${scss.link} ${activePage === '/contacts' && scss.activeBackground}`}
-									to="/contacts"
-									onClick={() => setActivePage('/contacts')}
-								>
-									Контакты
-								</Link>
-							</li>
-						</ul>
-						<div className={scss.userProfile}>
-							<div
-								className={scss.profileClick}
-								onClick={() => setIsMenu(!isMenu)}
-							>
-								<Profile />
-							</div>
-							<div>
-								{isMenu ? (
-									<div className={scss.isMenuProfile}>
-										<button>Войти</button>
-										<button>Регистрация</button>
-									</div>
-								) : null}
-							</div>
+						<nav>
+							<ul>
+								{links.map((item) => (
+									<li>
+										<Link to={item.link}>{item.label}</Link>
+									</li>
+								))}
+							</ul>
+						</nav>
+						<div>
+							<a href="#">+996 504 80 10 88</a>
 						</div>
 					</div>
-					<div className={scss.test}></div>
-					{/* //!HEADER FIXED */}
-					<div
-						className={
-							isScrolled
-								? `${scss.headerSecond} ${scss.active}`
-								: `${scss.headerSecond}`
-						}
-					>
-						<div className={scss.catalogSearch}>
-							{isScrolled && (
-								<Link to="/">
-									<IconGadgetarium />
-								</Link>
-							)}
-							<button className={scss.buttonCatalog}>
-								<EightPoints />
-								Каталог
-							</button>
-							<div className={scss.hr}></div>
-							<div>
-								<CustomSearch />
-							</div>
-						</div>
-						{!isScrolled && (
-							<div className={scss.iconsSocialNetwork}>
-								<IconFacebook />
-								<IconInstagram />
-								<IconWhatsapp />
-								{/* <span>0</span> */}
-							</div>
-						)}
-						<div className={scss.iconFavoriteBas}>
-							<IconComparison />
-							{/* <span>8</span>	 */}
-							<IconFavorite />
-							<IconBasket />
-						</div>
-					</div>
+					<div className={scss.sub_header}></div>
 				</div>
 			</div>
 		</header>
