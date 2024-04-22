3fc9f68eb1d2c96e75bd8bdaf64a3996740b7a0c
esenturDev <nurlanuuluesentur@gmail.com>
2024-04-22 15:56:04 +0600

    fixed

--- src/pagesUser/components/pages/fagSections/Fag.tsx
+++ src/pagesUser/components/pages/fagSections/Fag.tsx
@@ -1,8 +1,27 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
 import scss from './Fag.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { IconArrowRight } from '@tabler/icons-react';
 
+type ArrayTextTypes = {
+	text: string;
+	text2: string;
+	text3: string;
+	text4: string;
+};
+
+const ArrayText: ArrayTextTypes[] = [
+	{
+		text: 'Urna aliquet dignissim pharetra consectetur commodo pellentesque at tellus. Odio eget scelerisque faucibus ut. Cras amet, diam vel, sollicitudin gravida quis augue.',
+		text2:
+			'Bibendum et nulla accumsan sollicitudin aliquet velit viverra risus. Eget molestie enim quisque sodales habitant nulla tincidunt. Ullamcorper tincidunt purus sed facilisis.',
+		text3:
+			'	Ullamcorper aliquam gravida aliquet felis iaculis urna diam dui id. Consectetur in sagittis, sagittis, diam fames ac morbi convallis. Convallis vitae vel luctus erat tempus. Proin feugiat nisl egestas neque facilisi porta ipsum. At',
+		text4:
+			'		diam ut diam euismod sit fames tellus amet. In tortor placerat dictum faucibus id. Amet facilisis blandit dignissim sed nisl fermentum molestie pretium non.'
+	}
+];
 const Fag = () => {
 	const [openMenu1, setOpenMenu1] = useState<boolean>(false);
 	const [openMenu2, setOpenMenu2] = useState<boolean>(false);
@@ -16,6 +35,35 @@ const Fag = () => {
 	): void => {
 		setTimeout(() => {
 			valueBoolean(!value);
+		}, 355);
+	};
+
+	const handleOpenMenuResult2 = (
+		valueBoolean: React.Dispatch<React.SetStateAction<boolean>>
+	) => {
+		setTimeout(() => {
+			valueBoolean(false);
+		}, 355);
+	};
+	const handleOpenMenuResult3 = (
+		valueBoolean: React.Dispatch<React.SetStateAction<boolean>>
+	) => {
+		setTimeout(() => {
+			valueBoolean(false);
+		}, 355);
+	};
+	const handleOpenMenuResult4 = (
+		valueBoolean: React.Dispatch<React.SetStateAction<boolean>>
+	) => {
+		setTimeout(() => {
+			valueBoolean(false);
+		}, 355);
+	};
+	const handleOpenMenuResult5 = (
+		valueBoolean: React.Dispatch<React.SetStateAction<boolean>>
+	) => {
+		setTimeout(() => {
+			valueBoolean(false);
 		}, 355);
 	};
 
@@ -35,11 +83,17 @@ const Fag = () => {
 					<div className={scss.pageContent2}>
 						<h3>Часто задаваемые вопросы</h3>
 						<div className={scss.openMenuDiv}>
-							<div className={scss.containerMenu}>
-								<div
-									onClick={() => handleOpenMenu(setOpenMenu1, openMenu1)}
-									className={scss.MainContent}
-								>
+							<div
+								className={scss.containerMenu}
+								onClick={() => {
+									handleOpenMenu(setOpenMenu1, openMenu1);
+									handleOpenMenuResult2(setOpenMenu2);
+									handleOpenMenuResult3(setOpenMenu3);
+									handleOpenMenuResult4(setOpenMenu4);
+									handleOpenMenuResult5(setOpenMenu5);
+								}}
+							>
+								<div className={scss.MainContent}>
 									<div className={scss.content1}>
 										<div
 											className={
@@ -59,7 +113,7 @@ const Fag = () => {
 												: `${scss.iconNooActive}`
 										}
 									>
-										<IconArrowRight />
+										<IconArrowRight style={{ color: 'rgb(203, 17, 171)' }} />
 									</div>
 								</div>
 								<div
@@ -69,37 +123,27 @@ const Fag = () => {
 											: `${scss.divOpenMenuTexts}`
 									}
 								>
-									<p>
-										Urna aliquet dignissim pharetra consectetur commodo
-										pellentesque at tellus. Odio eget scelerisque faucibus ut.
-										Cras amet, diam vel, sollicitudin gravida quis augue.
-									</p>
-									<p>
-										{' '}
-										Bibendum et nulla accumsan sollicitudin aliquet velit
-										viverra risus. Eget molestie enim quisque sodales habitant
-										nulla tincidunt. Ullamcorper tincidunt purus sed facilisis.
-									</p>
-									<p>
-										{' '}
-										Ullamcorper aliquam gravida aliquet felis iaculis urna diam
-										dui id. Consectetur in sagittis, sagittis, diam fames ac
-										morbi convallis. Convallis vitae vel luctus erat tempus.
-										Proin feugiat nisl egestas neque facilisi porta ipsum. At
-									</p>
-									<p>
-										{' '}
-										diam ut diam euismod sit fames tellus amet. In tortor
-										placerat dictum faucibus id. Amet facilisis blandit
-										dignissim sed nisl fermentum molestie pretium non.
-									</p>
+									{ArrayText.map((item, index) => (
+										<>
+											<p key={index}>{item.text}</p>
+											<p key={index}>{item.text2}</p>
+											<p key={index}>{item.text3}</p>
+											<p key={index}>{item.text4}</p>
+										</>
+									))}
 								</div>
 							</div>
-							<div className={scss.containerMenu}>
-								<div
-									onClick={() => handleOpenMenu(setOpenMenu2, openMenu2)}
-									className={scss.MainContent}
-								>
+							<div
+								className={scss.containerMenu}
+								onClick={() => {
+									handleOpenMenu(setOpenMenu2, openMenu2);
+									handleOpenMenuResult2(setOpenMenu1);
+									handleOpenMenuResult3(setOpenMenu3);
+									handleOpenMenuResult4(setOpenMenu5);
+									handleOpenMenuResult5(setOpenMenu4);
+								}}
+							>
+								<div className={scss.MainContent}>
 									<div className={scss.content1}>
 										<div
 											className={
@@ -119,7 +163,7 @@ const Fag = () => {
 												: `${scss.iconNooActive}`
 										}
 									>
-										<IconArrowRight />
+										<IconArrowRight style={{ color: 'rgb(203, 17, 171)' }} />
 									</div>
 								</div>
 
@@ -130,37 +174,27 @@ const Fag = () => {
 											: `${scss.divOpenMenuTexts}`
 									}
 								>
-									<p>
-										Urna aliquet dignissim pharetra consectetur commodo
-										pellentesque at tellus. Odio eget scelerisque faucibus ut.
-										Cras amet, diam vel, sollicitudin gravida quis augue.
-									</p>
-									<p>
-										{' '}
-										Bibendum et nulla accumsan sollicitudin aliquet velit
-										viverra risus. Eget molestie enim quisque sodales habitant
-										nulla tincidunt. Ullamcorper tincidunt purus sed facilisis.
-									</p>
-									<p>
-										{' '}
-										Ullamcorper aliquam gravida aliquet felis iaculis urna diam
-										dui id. Consectetur in sagittis, sagittis, diam fames ac
-										morbi convallis. Convallis vitae vel luctus erat tempus.
-										Proin feugiat nisl egestas neque facilisi porta ipsum. At
-									</p>
-									<p>
-										{' '}
-										diam ut diam euismod sit fames tellus amet. In tortor
-										placerat dictum faucibus id. Amet facilisis blandit
-										dignissim sed nisl fermentum molestie pretium non.
-									</p>
+									{ArrayText.map((item, index) => (
+										<>
+											<p key={index}>{item.text}</p>
+											<p key={index}>{item.text2}</p>
+											<p key={index}>{item.text3}</p>
+											<p key={index}>{item.text4}</p>
+										</>
+									))}
 								</div>
 							</div>
-							<div className={scss.containerMenu}>
-								<div
-									onClick={() => handleOpenMenu(setOpenMenu3, openMenu3)}
-									className={scss.MainContent}
-								>
+							<div
+								className={scss.containerMenu}
+								onClick={() => {
+									handleOpenMenu(setOpenMenu3, openMenu3);
+									handleOpenMenuResult2(setOpenMenu1);
+									handleOpenMenuResult3(setOpenMenu2);
+									handleOpenMenuResult4(setOpenMenu5);
+									handleOpenMenuResult5(setOpenMenu4);
+								}}
+							>
+								<div className={scss.MainContent}>
 									<div className={scss.content1}>
 										<div
 											className={
@@ -182,7 +216,7 @@ const Fag = () => {
 												: `${scss.iconNooActive}`
 										}
 									>
-										<IconArrowRight />
+										<IconArrowRight style={{ color: 'rgb(203, 17, 171)' }} />
 									</div>
 								</div>
 
@@ -193,37 +227,27 @@ const Fag = () => {
 											: `${scss.divOpenMenuTexts}`
 									}
 								>
-									<p>
-										Urna aliquet dignissim pharetra consectetur commodo
-										pellentesque at tellus. Odio eget scelerisque faucibus ut.
-										Cras amet, diam vel, sollicitudin gravida quis augue.
-									</p>
-									<p>
-										{' '}
-										Bibendum et nulla accumsan sollicitudin aliquet velit
-										viverra risus. Eget molestie enim quisque sodales habitant
-										nulla tincidunt. Ullamcorper tincidunt purus sed facilisis.
-									</p>
-									<p>
-										{' '}
-										Ullamcorper aliquam gravida aliquet felis iaculis urna diam
-										dui id. Consectetur in sagittis, sagittis, diam fames ac
-										morbi convallis. Convallis vitae vel luctus erat tempus.
-										Proin feugiat nisl egestas neque facilisi porta ipsum. At
-									</p>
-									<p>
-										{' '}
-										diam ut diam euismod sit fames tellus amet. In tortor
-										placerat dictum faucibus id. Amet facilisis blandit
-										dignissim sed nisl fermentum molestie pretium non.
-									</p>
+									{ArrayText.map((item, index) => (
+										<>
+											<p key={index}>{item.text}</p>
+											<p key={index}>{item.text2}</p>
+											<p key={index}>{item.text3}</p>
+											<p key={index}>{item.text4}</p>
+										</>
+									))}
 								</div>
 							</div>
-							<div className={scss.containerMenu}>
-								<div
-									onClick={() => handleOpenMenu(setOpenMenu4, openMenu4)}
-									className={scss.MainContent}
-								>
+							<div
+								className={scss.containerMenu}
+								onClick={() => {
+									handleOpenMenu(setOpenMenu4, openMenu4);
+									handleOpenMenuResult2(setOpenMenu1);
+									handleOpenMenuResult3(setOpenMenu3);
+									handleOpenMenuResult4(setOpenMenu5);
+									handleOpenMenuResult5(setOpenMenu2);
+								}}
+							>
+								<div className={scss.MainContent}>
 									<div className={scss.content1}>
 										<div
 											className={
@@ -245,7 +269,7 @@ const Fag = () => {
 												: `${scss.iconNooActive}`
 										}
 									>
-										<IconArrowRight />
+										<IconArrowRight style={{ color: 'rgb(203, 17, 171)' }} />
 									</div>
 								</div>
 
@@ -256,37 +280,27 @@ const Fag = () => {
 											: `${scss.divOpenMenuTexts}`
 									}
 								>
-									<p>
-										Urna aliquet dignissim pharetra consectetur commodo
-										pellentesque at tellus. Odio eget scelerisque faucibus ut.
-										Cras amet, diam vel, sollicitudin gravida quis augue.
-									</p>
-									<p>
-										{' '}
-										Bibendum et nulla accumsan sollicitudin aliquet velit
-										viverra risus. Eget molestie enim quisque sodales habitant
-										nulla tincidunt. Ullamcorper tincidunt purus sed facilisis.
-									</p>
-									<p>
-										{' '}
-										Ullamcorper aliquam gravida aliquet felis iaculis urna diam
-										dui id. Consectetur in sagittis, sagittis, diam fames ac
-										morbi convallis. Convallis vitae vel luctus erat tempus.
-										Proin feugiat nisl egestas neque facilisi porta ipsum. At
-									</p>
-									<p>
-										{' '}
-										diam ut diam euismod sit fames tellus amet. In tortor
-										placerat dictum faucibus id. Amet facilisis blandit
-										dignissim sed nisl fermentum molestie pretium non.
-									</p>
+									{ArrayText.map((item, index) => (
+										<>
+											<p key={index}>{item.text}</p>
+											<p key={index}>{item.text2}</p>
+											<p key={index}>{item.text3}</p>
+											<p key={index}>{item.text4}</p>
+										</>
+									))}
 								</div>
 							</div>
-							<div className={scss.containerMenu}>
-								<div
-									onClick={() => handleOpenMenu(setOpenMenu5, openMenu5)}
-									className={scss.MainContent}
-								>
+							<div
+								className={scss.containerMenu}
+								onClick={() => {
+									handleOpenMenu(setOpenMenu5, openMenu5);
+									handleOpenMenuResult2(setOpenMenu1);
+									handleOpenMenuResult3(setOpenMenu3);
+									handleOpenMenuResult4(setOpenMenu2);
+									handleOpenMenuResult5(setOpenMenu4);
+								}}
+							>
+								<div className={scss.MainContent}>
 									<div className={scss.content1}>
 										<div
 											className={
@@ -308,7 +322,7 @@ const Fag = () => {
 												: `${scss.iconNooActive}`
 										}
 									>
-										<IconArrowRight />
+										<IconArrowRight style={{ color: 'rgb(203, 17, 171)' }} />
 									</div>
 								</div>
 								{openMenu5 && (
@@ -319,32 +333,14 @@ const Fag = () => {
 												: `${scss.divOpenMenuTexts}`
 										}
 									>
-										<p>
-											Urna aliquet dignissim pharetra consectetur commodo
-											pellentesque at tellus. Odio eget scelerisque faucibus ut.
-											Cras amet, diam vel, sollicitudin gravida quis augue.
-										</p>
-										<p>
-											{' '}
-											Bibendum et nulla accumsan sollicitudin aliquet velit
-											viverra risus. Eget molestie enim quisque sodales habitant
-											nulla tincidunt. Ullamcorper tincidunt purus sed
-											facilisis.
-										</p>
-										<p>
-											{' '}
-											Ullamcorper aliquam gravida aliquet felis iaculis urna
-											diam dui id. Consectetur in sagittis, sagittis, diam fames
-											ac morbi convallis. Convallis vitae vel luctus erat
-											tempus. Proin feugiat nisl egestas neque facilisi porta
-											ipsum. At
-										</p>
-										<p>
-											{' '}
-											diam ut diam euismod sit fames tellus amet. In tortor
-											placerat dictum faucibus id. Amet facilisis blandit
-											dignissim sed nisl fermentum molestie pretium non.
-										</p>
+										{ArrayText.map((item, index) => (
+											<>
+												<p key={index}>{item.text}</p>
+												<p key={index}>{item.text2}</p>
+												<p key={index}>{item.text3}</p>
+												<p key={index}>{item.text4}</p>
+											</>
+										))}
 									</div>
 								)}
 							</div>
