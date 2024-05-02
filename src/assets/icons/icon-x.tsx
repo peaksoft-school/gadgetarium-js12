a798d4a6b38855b4292f87ef91086218ecfbb708
NuriFront <toktogulovanurzanat@gmail.com>
2024-05-02 14:39:51 +0600

    added comparison page

---
+++ src/assets/icons/icon-x.tsx
@@ -0,0 +1,44 @@
+const iconX = () => {
+	return (
+		<>
+			<svg
+				width="16.000000"
+				height="16.000000"
+				viewBox="0 0 16 16"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<desc>Created with Pixso.</desc>
+				<defs>
+					<clipPath id="clip3_27457">
+						<rect
+							id="delete (2) 1"
+							width="16.000000"
+							height="16.000046"
+							fill="white"
+							fill-opacity="0"
+						/>
+					</clipPath>
+				</defs>
+				<rect
+					id="delete (2) 1"
+					width="16.000000"
+					height="16.000046"
+					fill="#FFFFFF"
+					fill-opacity="0"
+				/>
+				<g clip-path="url(#clip3_27457)">
+					<path
+						id="Vector"
+						d="M11.69 5.57L9.27 8L11.69 10.42C12.05 10.78 12.05 11.36 11.69 11.72C11.51 11.9 11.27 12 11.04 12C10.8 12 10.56 11.91 10.38 11.72L7.96 9.3L5.54 11.72C5.36 11.9 5.12 12 4.88 12C4.65 12 4.41 11.91 4.23 11.72C3.87 11.36 3.87 10.78 4.23 10.42L6.65 8L4.23 5.57C3.87 5.21 3.87 4.63 4.23 4.27C4.59 3.91 5.17 3.91 5.54 4.27L7.96 6.69L10.38 4.27C10.74 3.91 11.33 3.91 11.69 4.27C12.05 4.63 12.05 5.21 11.69 5.57Z"
+						fill="#909CB5"
+						fill-opacity="1.000000"
+						fill-rule="nonzero"
+					/>
+				</g>
+			</svg>
+		</>
+	);
+};
+
+export default iconX;
